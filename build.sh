c6d2c11520fe86906a9ce80759e6cbe4e0f9a5f1
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-28 20:12:13 +0100

    Tidying build script

--- build.sh
+++ build.sh
@@ -1,53 +1,59 @@
-#!/usr/bin/env sh
-set -eux
+#!/bin/sh
+#
+# Download and build haproxy container
+set -euo pipefail
 
-HAPROXY_VERSION=1.6.7
-HAPROXY_SHA256=583e0c0c3388c0597dea241601f3fedfe1d7ff8c735d471831be67315f58183a
+version=1.6.7
+sha256=583e0c0c3388c0597dea241601f3fedfe1d7ff8c735d471831be67315f58183a
 
-BASE=$PWD
-SRC=$PWD/src
-OUT=$PWD/out
-ROOTFS=$PWD/rootfs
+src=$PWD/haproxy
+out=$PWD/out
+rootfs=$PWD/rootfs
 
-mkdir -p $OUT
+container=$PWD/container
 
-mkdir -p $BASE/haproxy
-cd $BASE
-curl -OL http://www.haproxy.org/download/1.6/src/haproxy-$HAPROXY_VERSION.tar.gz
-echo "$HAPROXY_SHA256  haproxy-$HAPROXY_VERSION.tar.gz" | sha256sum -c
-tar -C $BASE/haproxy --strip-components 1 -xf haproxy-$HAPROXY_VERSION.tar.gz
+slz=$PWD/libslz
+lua=$PWD/liblua
+libressl=$PWD/libressl
 
-cd $BASE/haproxy
-make TARGET=linux2628 \
-       USE_TFO=1 \
-       CPU=native CPU_CFLAGS.native=-O3 \
-       USE_PCRE_JIT=1 \
-       ADDLIB="-static -L$BASE/libslz-build/lib" \
-       USE_SLZ=1 ADDINC=-I$BASE/libslz-build/include \
-       USE_LUA=yes LUA_LIB_NAME=lua LUA_LIB=$BASE/lua-build/lib LUA_INC=$BASE/lua-build/include \
-       USE_OPENSSL=1 SSL_LIB=$BASE/libressl/lib SSL_INC=$BASE/libressl/include
+download() {
+  mkdir -p ${src}
+  curl -OL http://www.haproxy.org/download/1.6/src/haproxy-${version}.tar.gz
+  echo "${sha256}  haproxy-${version}.tar.gz" | sha256sum -c
+  tar -C ${src} --strip-components 1 -xf haproxy-${version}.tar.gz
+}
 
-mkdir -p $ROOTFS/bin $ROOTFS/var/run/haproxy
-
-cp -r $SRC/etc $ROOTFS
-
-cp haproxy haproxy-systemd-wrapper $ROOTFS/bin
-
-cat <<EOF > $ROOTFS/etc/passwd
+build() {
+  cd ${src}
+  make TARGET=linux2628 \
+         USE_TFO=1 \
+         CPU=native CPU_CFLAGS.native=-O3 \
+         USE_PCRE_JIT=1 \
+         ADDLIB="-static -L${slz}/lib" USE_SLZ=1 ADDINC=-I${slz}/include \
+         USE_LUA=yes LUA_LIB_NAME=lua LUA_LIB=${lua}/lib LUA_INC=${lua}/include \
+         USE_OPENSSL=1 SSL_LIB=${libressl}/lib SSL_INC=${libressl}/include
+  
+  mkdir -p ${rootfs}/bin ${rootfs}/var/run/haproxy
+  
+  cp haproxy haproxy-systemd-wrapper ${rootfs}/bin
+  
+  cp -r ${container}/etc ${rootfs}
+  
+  cat <<EOF > ${rootfs}/etc/passwd
 root:x:0:0:root:/:/dev/null
 nobody:x:65534:65534:nogroup:/:/dev/null
 EOF
 
-cat <<EOF > $ROOTFS/etc/group
+  cat <<EOF > ${rootfs}/etc/group
 root:x:0:
 nogroup:x:65534:
 EOF
+  
+  tar -cf ${out}/rootfs.tar -C ${rootfs} .  
+}
 
-
-cd $ROOTFS
-tar -cf $OUT/rootfs.tar .
-
-cat <<EOF > $OUT/Dockerfile
+dockerfile() {  
+  cat <<EOF > ${out}/Dockerfile
 FROM scratch
 
 ADD rootfs.tar /
@@ -56,3 +62,8 @@ ENTRYPOINT [ "/bin/haproxy-systemd-wrapper", "-p", "/var/run/haproxy.pid" ]
 CMD        [ "-f", "/etc/haproxy.cfg" ]
 
 EOF
+}
+
+download
+build
+dockerfile
