7b33a9acf57c1d4772ead1a49c8a354b434486d3
Markus Kobler <markus@distinctive.co>
2016-10-02 11:20:15 +0100

    Bumping to haproxy 1.6.9

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Download and build haproxy container
 set -euo pipefail
 
-version=1.6.7
-sha256=583e0c0c3388c0597dea241601f3fedfe1d7ff8c735d471831be67315f58183a
-
-src=$PWD/haproxy
+src=$PWD/src
 out=$PWD/out
 rootfs=$PWD/rootfs
 
@@ -16,14 +13,19 @@ slz=$PWD/libslz
 lua=$PWD/liblua
 libressl=$PWD/libressl
 
-download() {
+# _download "version" "sha256"
+_download() {
   mkdir -p ${src}
-  curl -OL http://www.haproxy.org/download/1.6/src/haproxy-${version}.tar.gz
-  echo "${sha256}  haproxy-${version}.tar.gz" | sha256sum -c
-  tar -C ${src} --strip-components 1 -xf haproxy-${version}.tar.gz
+  curl -OL http://www.haproxy.org/download/1.6/src/haproxy-${1}.tar.gz
+  echo "${2}  haproxy-${1}.tar.gz" | sha256sum -c
+  tar -C ${src} --strip-components 1 -xf haproxy-${1}.tar.gz
+  
+  cat <<EOF > ${out}/version
+${1}
+EOF
 }
 
-build() {
+_build() {
   cd ${src}
   make TARGET=linux2628 \
          USE_TFO=1 \
@@ -32,13 +34,13 @@ build() {
          ADDLIB="-static -L${slz}/lib" USE_SLZ=1 ADDINC=-I${slz}/include \
          USE_LUA=yes LUA_LIB_NAME=lua LUA_LIB=${lua}/lib LUA_INC=${lua}/include \
          USE_OPENSSL=1 SSL_LIB=${libressl}/lib SSL_INC=${libressl}/include
-  
+
   mkdir -p ${rootfs}/bin ${rootfs}/var/run/haproxy
-  
+
   cp haproxy haproxy-systemd-wrapper ${rootfs}/bin
-  
+
   cp -r ${container}/etc ${rootfs}
-  
+
   cat <<EOF > ${rootfs}/etc/passwd
 root:x:0:0:root:/:/dev/null
 nobody:x:65534:65534:nogroup:/:/dev/null
@@ -48,11 +50,12 @@ EOF
 root:x:0:
 nogroup:x:65534:
 EOF
-  
-  tar -cf ${out}/rootfs.tar -C ${rootfs} .  
+
+  tar -cf ${out}/rootfs.tar -C ${rootfs} .
 }
 
-dockerfile() {  
+# _dockerfile "version"
+_dockerfile() {
   cat <<EOF > ${out}/Dockerfile
 FROM scratch
 
@@ -64,6 +67,6 @@ CMD        [ "-f", "/etc/haproxy.cfg" ]
 EOF
 }
 
-download
-build
-dockerfile
+_download 1.6.9 cf7d2fa891d2ae4aa6489fc43a9cadf68c42f9cb0de4801afad45d32e7dda133
+_build
+_dockerfile
