fa6623563b6e48b0549b6c722fd1d28480d2dfa8
Markus Kobler <markus@distinctive.co>
2017-03-03 20:52:31 -0500

    Bump version

--- build.sh
+++ build.sh
@@ -10,8 +10,29 @@ rootfs=$PWD/rootfs
 container=$PWD/container
 
 slz=$PWD/libslz
-lua=$PWD/liblua
 libressl=$PWD/libressl
+
+_init() {
+  apk add --no-cache --no-progress \
+      autoconf \
+      automake \
+      bash \
+      cmake \
+      curl \
+      gcc \
+      git \
+      gnupg1 \
+      libc-dev \
+      libtool \
+      linux-headers \
+      make \
+      pcre-dev \
+      perl \
+      readline-dev \
+      tar \
+      xz-dev \
+      zlib-dev
+}
 
 # _download "version" "sha256"
 _download() {
@@ -19,7 +40,7 @@ _download() {
   curl -OL http://www.haproxy.org/download/1.6/src/haproxy-${1}.tar.gz
   echo "${2}  haproxy-${1}.tar.gz" | sha256sum -c
   tar -C ${src} --strip-components 1 -xf haproxy-${1}.tar.gz
-  
+
   cat <<EOF > ${out}/version
 ${1}
 EOF
@@ -32,7 +53,6 @@ _build() {
          CPU=native CPU_CFLAGS.native=-O3 \
          USE_PCRE_JIT=1 \
          ADDLIB="-static -L${slz}/lib" USE_SLZ=1 ADDINC=-I${slz}/include \
-         USE_LUA=yes LUA_LIB_NAME=lua LUA_LIB=${lua}/lib LUA_INC=${lua}/include \
          USE_OPENSSL=1 SSL_LIB=${libressl}/lib SSL_INC=${libressl}/include
 
   mkdir -p ${rootfs}/bin ${rootfs}/var/run/haproxy
@@ -43,12 +63,12 @@ _build() {
 
   cat <<EOF > ${rootfs}/etc/passwd
 root:x:0:0:root:/:/dev/null
-nobody:x:65534:65534:nogroup:/:/dev/null
+nobody:x:65534:65534:nobody:/:/dev/null
 EOF
 
   cat <<EOF > ${rootfs}/etc/group
 root:x:0:
-nogroup:x:65534:
+nobody:x:65534:
 EOF
 
   tar -cf ${out}/rootfs.tar -C ${rootfs} .
@@ -67,6 +87,7 @@ CMD        [ "-f", "/etc/haproxy.cfg" ]
 EOF
 }
 
-_download 1.6.9 cf7d2fa891d2ae4aa6489fc43a9cadf68c42f9cb0de4801afad45d32e7dda133
+_init
+_download 1.7.3 ebb31550a5261091034f1b6ac7f4a8b9d79a8ce2a3ddcd7be5b5eb355c35ba65
 _build
 _dockerfile
